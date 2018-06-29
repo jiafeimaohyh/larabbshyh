e7e5107104fdd2740d82bc13559cc0940a5e2eec
jiafeimao <1157873987@qq.com>
2018-06-29 08:46:57 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,20 +9,49 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+	public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
+    /**
+     * [show 个人界面展示]
+     * @desc   描述
+     * @author 加菲猫
+     * @param  User   $user [description]
+     * @return [type]       [description]
+     */
     public function show(User $user)
     {
     	return view('users.show', compact('user'));
     }
-
+    /**
+     * [edit 个人信息编辑]
+     * @desc   描述
+     * @author 加菲猫
+     * @param  User   $user [description]
+     * @return [type]       [description]
+     */
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
-
+    /**
+     * [update 个人信息编辑操作]
+     * @desc   描述
+     * @author 加菲猫
+     * @param  UserRequest        $request  [description]
+     * @param  ImageUploadHandler $uploader [description]
+     * @param  User               $user     [description]
+     * @return [type]                       [description]
+     */
  	public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+    	$this->authorize('update', $user);
         $data = $request->all();
-
+        //上传图片
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
             if ($result) {
