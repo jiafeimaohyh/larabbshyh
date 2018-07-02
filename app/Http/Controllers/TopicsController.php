7992c3dbb1457843d250505f1236c8e3384353b1
jiafeimao <1157873987@qq.com>
2018-07-02 12:51:47 +0000

    User can create topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -26,15 +26,19 @@ class TopicsController extends Controller
     }
 
 	public function create(Topic $topic)
-	{
-		return view('topics.create_and_edit', compact('topic'));
-	}
+    {
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function store(TopicRequest $request)
-	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+    public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    }
 
 	public function edit(Topic $topic)
 	{
