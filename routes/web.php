056d99e7a9953d67c7ee7c8b10aeb14df8c2a84b
jiafeimao <1157873987@qq.com>
2018-07-02 01:45:07 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,5 @@ Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
