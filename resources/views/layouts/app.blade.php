e6e99a7a27abfa83aaaf31da4e8f58e68bb23a25
jiafeimao <1157873987@qq.com>
2018-07-03 13:48:52 +0000

    Add manage contents permission and visit horizon dashboard permission

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,10 @@
 
     </div>
 
+    @if (app()->isLocal())
+        @include('sudosu::user-selector')
+    @endif
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('scripts')
