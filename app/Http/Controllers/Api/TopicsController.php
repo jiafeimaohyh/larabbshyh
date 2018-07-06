854cf64a2d6f89a660d72e16c1080e95820f4dda
jiafeimao <1157873987@qq.com>
2018-07-06 08:35:31 +0000

    topic index

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -6,7 +6,7 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Transformers\TopicTransformer;
 use App\Http\Requests\Api\TopicRequest;
-
+use App\Models\User;
 class TopicsController extends Controller
 {
 
@@ -59,6 +59,54 @@ class TopicsController extends Controller
 
 	    $topic->delete();
 	    return $this->response->noContent();
+	}
+
+	/**
+	 * [index 话题列表]
+	 * @desc   描述
+	 * @author 加菲猫
+	 * @param  Request $request [description]
+	 * @param  Topic   $topic   [description]
+	 * @return [type]           [description]
+	 */
+	public function index(Request $request, Topic $topic)
+    {
+        $query = $topic->query();
+
+        if ($categoryId = $request->category_id) {
+            $query->where('category_id', $categoryId);
+        }
+
+        // 为了说明 N+1问题，不使用 scopeWithOrder
+        switch ($request->order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
+    /**
+     * [userIndex 某个用户发布的话题]
+     * @desc   描述
+     * @author 加菲猫
+     * @param  User    $user    [description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function userIndex(User $user, Request $request)
+	{
+	    $topics = $user->topics()->recent()
+	        ->paginate(20);
+
+	    return $this->response->paginator($topics, new TopicTransformer());
 	}
 
 
--- routes/api.php
+++ routes/api.php
@@ -89,6 +89,13 @@ $api->version('v1',
 		// 游客可以访问的接口
 		$api->get('categories', 'CategoriesController@index')
 		    ->name('api.categories.index');
+		// 话题列表
+	    $api->get('topics', 'TopicsController@index')
+    		->name('api.topics.index');
+    	// 某个用户发布的话题
+		$api->get('users/{user}/topics', 'TopicsController@userIndex')
+		    ->name('api.users.topics.index');
+
 
 		// 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
@@ -112,7 +119,7 @@ $api->version('v1',
 			// 修改话题
 		    $api->patch('topics/{topic}', 'TopicsController@update')
     			->name('api.topics.update');
-    			
+
     		// 删除话题
 			$api->delete('topics/{topic}', 'TopicsController@destroy')
 			    ->name('api.topics.destroy');
