d1bfbda522a404c6cc75ea9142415dd53dfeae38
jiafeimao <1157873987@qq.com>
2018-07-07 00:57:58 +0000

    notifications read

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -7,6 +7,12 @@ use App\Transformers\NotificationTransformer;
 
 class NotificationsController extends Controller
 {
+	/**
+	 * [index 消息列表]
+	 * @desc   描述
+	 * @author 加菲猫
+	 * @return [type] [description]
+	 */
     public function index()
     {
         $notifications = $this->user->notifications()->paginate(20);
@@ -14,11 +20,29 @@ class NotificationsController extends Controller
         return $this->response->paginator($notifications, new NotificationTransformer());
     }
 
-
+    /**
+     * [stats 返回未读消息数量]
+     * @desc   描述
+     * @author 加菲猫
+     * @return [type] [description]
+     */
     public function stats()
 	{
 	    return $this->response->array([
 	        'unread_count' => $this->user()->notification_count,
 	    ]);
+	}
+
+	/**
+	 * [read 标志消息为已读]
+	 * @desc   描述
+	 * @author 加菲猫
+	 * @return [type] [description]
+	 */
+	public function read()
+	{
+	    $this->user()->markAsRead();
+
+	    return $this->response->noContent();
 	}
 }
